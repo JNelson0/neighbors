447a2f0af2f3fb48e49ef7c94032067ea35f8eca
Jared Nelson <jaredlnelson1@gmail.com>
2022-08-10 13:47:21 -0400

    Routing Setup and Component Creation
    Server needs to be setup next

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import MessagePage from "./components/messagePage/MessagePage";
+import Settings from "./components/settings/Settings";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App d-flex align-items-center justify-content-center">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/message" element={<MessagePage />} />
+					<Route path="/settings" element={<Settings />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ client/src/components/login/Login.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./login.scss";
+
+import { Container, Button } from "react-bootstrap";
+
+export default function Login() {
+	return (
+		<div>
+			<Container fluid>
+				<Button variant="outline-primary">Button</Button>
+			</Container>
+		</div>
+	);
+}
