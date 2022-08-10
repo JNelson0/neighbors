6bb6898a9cfb1827920f9c74b63f3e9ad338f3eb
Jared Nelson <jaredlnelson1@gmail.com>
2022-08-10 13:55:24 -0400

    1st push

--- client/src/components/messagePage/MessagePage.tsx
+++ client/src/components/messagePage/MessagePage.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import "./messagePage.scss";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function MessagePage() {
-	return <div>MessagePage</div>;
+	return (
+		<div className="message">
+			<Container fluid>
+				<Row>
+					<Col style={{ backgroundColor: "Blue" }}> asdf</Col>
+					<Col style={{ backgroundColor: "Green" }}>asdf </Col>
+				</Row>
+				<Row>
+					<Col style={{ backgroundColor: "red" }}> asdf</Col>
+					<Col style={{ backgroundColor: "black" }}>asdf </Col>
+				</Row>
+			</Container>
+		</div>
+	);
 }
